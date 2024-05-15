c42933d5ce4196b1b59af54c160f6e136284226f
Ashraf Aaref <82468597+AshrafAaref21@users.noreply.github.com>
2024-05-15 21:25:01 +0300

    Add files via upload

--- predictor.py
+++ predictor.py
@@ -1,9 +1,11 @@
 import requests
 import streamlit as st
-from utils import read_response, to_date, visual
+from pandas import concat
+
+from utils import read_response, to_date, visual, visual_compare
 
 
-def output(response, data):
+def output(response, data, callback=None):
     if str(response.status_code)[0] != '2':
         st.error(f'Bad Request with status code\
                      {response.status_code}\
@@ -20,8 +22,11 @@ def output(response, data):
 
     if str(response.status_code)[0] == '2':
         df = read_response(response=response)
+        df = df[['date', 'profit_short', 'profit_long', 'profit_total',
+                 'mwh_total', 'win_count']]
         if len(df) > 0:
             df['date'] = df['date'].apply(to_date)
+
             st.dataframe(df)
 
             df["profit_total"] = df["profit_total"].cumsum()
@@ -30,32 +35,43 @@ def output(response, data):
 
             st.divider()
 
-            visual(df)
+            if callback is not None:
+                callback(df)
         else:
             st.info("There's no Records for this short period. :anchor:")
+
+        return df
 
 
 def single_model(data):
     response = requests.post(
         'https://quantum-zero-bayfm.ondigitalocean.app/report', data=data)
 
-    output(response, data)
+    output(response, data, visual)
 
 
 def models_comparison(data1, data2):
-    cl1, _, cl2 = st.columns([5, 1, 5])
-
     response_1 = requests.post(
         'https://quantum-zero-bayfm.ondigitalocean.app/report', data=data1)
-    
+
     response_2 = requests.post(
         'https://quantum-zero-bayfm.ondigitalocean.app/report', data=data2)
+
+    cl1, _, cl2 = st.columns([5, 1, 5])
+
     st.divider()
 
     with cl1:
-
-        output(response_1, data1)
-
+        st.subheader(f"Model {data1['model']}")
+        df1 = output(response_1, data1)
+        df1.set_index('date', inplace=True)
+        df1.columns = [f"{i} ({data1['model']})" for i in df1.columns]
     with cl2:
+        st.subheader(f"Model {data2['model']}")
+        df2 = output(response_2, data2)
+        df2.set_index('date', inplace=True)
+        df2.columns = [f"{i} ({data2['model']})" for i in df2.columns]
 
-        output(response_2, data2)
+    df = df1.join(df2)
+    st.dataframe(df)
+    visual_compare(df, data1, data2)
--- utils.py
+++ utils.py
@@ -56,6 +56,73 @@ def visual(df):
         st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 
+def visual_compare(df, data1, data2):
+    tab1, tab2 = st.tabs(["Plot Profit Cumulative", "Wins VS Losses"])
+    with tab1:
+
+        fig = px.line(
+            df.reset_index(),
+            x='date',
+            y=[
+                f"profit_short ({data1['model']})", f"profit_long ({data1['model']})", f"profit_total ({data1['model']})",
+                f"profit_short ({data2['model']})", f"profit_long ({data2['model']})", f"profit_total ({data2['model']})"
+            ],
+            labels={'value': 'Profit Cumulative', 'variable': ''},
+
+        )
+
+        newnames = {f"profit_total ({data1['model']})": f"Total ({data1['model']})",
+                    f"profit_long ({data1['model']})": f"Long ({data1['model']})",
+                    f"profit_short ({data1['model']})": f"Short ({data1['model']})",
+                    f"profit_total ({data2['model']})": f"Total ({data2['model']})",
+                    f"profit_long ({data2['model']})": f"Long ({data2['model']})",
+                    f"profit_short ({data2['model']})": f"Short ({data2['model']})",
+                    }
+
+        fig.for_each_trace(lambda t: t.update(name=newnames[t.name]))
+
+        fig.update_layout(
+            hovermode="x"
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    with tab2:
+        # y_max = np.ceil(df.win_count.max() / 5) * 5 + 5
+        # if df.win_count.max() < df.loss_count.max():
+        #     y_max = np.ceil(df.loss_count.max() / 5) * 5 + 5
+        # y_min = np.floor(df.win_count.min() / 5) * 5 - 5
+        # if df.win_count.min() < df.loss_count.min():
+        #     y_min = np.floor(df.loss_count.min() / 5) * 5 - 5
+
+        fig = px.line(
+            df.reset_index(),
+            x='date',
+            y=[
+                f"mwh_total ({data1['model']})", f"win_count ({data1['model']})",
+                f"mwh_total ({data2['model']})", f"win_count ({data2['model']})"
+            ],
+            # color_discrete_sequence=px.colors.qualitative.Antique,
+            # range_y=[y_min, y_max],
+            labels={'value': 'Wins and Losses',
+                    'variable': ''}
+        )
+        newnames = {
+            f"mwh_total ({data1['model']})": f"# Economics ({data1['model']})",
+            f"win_count ({data1['model']})": f"# Wins ({data1['model']})",
+            f"mwh_total ({data2['model']})": f"# Economics ({data2['model']})",
+            f"win_count ({data2['model']})": f"# Wins ({data2['model']})",
+
+        }
+
+        fig.for_each_trace(lambda t: t.update(name=newnames[t.name]))
+        fig.update_layout(
+            hovermode="x"
+        )
+
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+
 def to_date(date):
     return (datetime(1970, 1, 1) + timedelta(days=date)).strftime('%Y-%m-%d')
 
